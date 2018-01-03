0b3cc8047ed8a2bef2b6084945005ee716ad6c59
Robby <fzpinus@gmail.com>
2018-01-03 12:03:29 -0800

    feat(167): Two Sum II - Input array is sorted

---
+++ 配列/167_Two Sum II - Input array is sorted.py
@@ -0,0 +1,11 @@
+# 167_Two Sum II - Input array is sorted
+# 和1类似, 每次遇到, 存入d{}
+# 遇到新数, 检查他的互补数是否在d中, 如果有, 返回即可
+
+class Solution:
+  def twoSum(self, numbers, target):
+    d = {}
+    for i, num in enumerate(numbers):
+      if target - num in d:
+        return [d[target - num], i]
+      d[num] = i
